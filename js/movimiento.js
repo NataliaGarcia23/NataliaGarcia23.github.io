0f661eb1d8f6542e011cd3f5687bd3f8a24d37ff
NataliaGarcia23 <72567288+NataliaGarcia23@users.noreply.github.com>
2023-12-15 20:47:19 +0100

    Add files via upload

---
+++ js/movimiento.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const profileContainer = document.querySelector(".profile-container");
+    if (profileContainer) {
+      profileContainer.classList.add("show");
+    }
+  
+    const boxContainer = document.querySelector(".box");
+    if (boxContainer) {
+      boxContainer.classList.add("show");
+    }
+    
+});
